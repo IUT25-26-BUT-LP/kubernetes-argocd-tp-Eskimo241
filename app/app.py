a22596f63d6cc0bb30113d599a94ea8823929947
Eskimo241 <eskimo241pro@gmail.com>
2026-05-07 13:09:20 +0200

    runner fix

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Statut : Pipeline DevSecOps Opérationnel !</h1><p>Image vérifiée et déployée par ArgoCD.Salut</p>"
+    return "<h1>Statut : Pipeline DevSecOps Opérationnel !</h1><p>Image vérifiée et déployée par ArgoCD.Salut2</p>"
 
 if __name__ == "__main__":
     # On écoute sur le port 8080
